b59a6c258fc7a0d973b8a0300ec7c599540effa1
Musa Ahmetcan Türker <ahmetcantrker@yahoo.com>
2017-11-30 18:12:27 +0300

    i

---
+++ src/webz-sgml/Sgml/HtmlConverter.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Xml;
+
+using Sgml;
+using Webz.Core;
+
+namespace Webz.Sgml
+{
+    public class HtmlConverter : IHtmlConverter
+    {
+        private readonly SgmlReader _sgmlReader;
+
+        public HtmlConverter()
+        {
+            _sgmlReader = new SgmlReader
+            {
+                DocType = "HTML",
+                WhitespaceHandling = WhitespaceHandling.All,
+                CaseFolding = CaseFolding.ToLower
+            };
+        }
+
+        public XmlDocument ToXmlDocument(string html)
+        {
+            _sgmlReader.InputStream = new StringReader(html);
+
+            var xmlDocument = new XmlDocument
+            {
+                PreserveWhitespace = true,
+                XmlResolver = null
+            };
+
+            xmlDocument.Load(_sgmlReader);
+
+            return xmlDocument;
+        }
+    }
+}
